b7feff41c2c30e457f3718369a6ce94c7a6a2fb2
Adrian <adicirstei@gmail.com>
2014-05-16 15:31:54 +0300

    g

--- gulpfile.js
+++ gulpfile.js
@@ -8,3 +8,18 @@ gulp.task('default', function() {
 gulp.task('build', function() {
   // place code for your default task here
 });
+
+gulp.task('buildbranch', function() {
+  var buildBranch = require('buildbranch');
+  buildBranch({
+    branch: 'master',
+    ignore: ['.git', 'www', 'node_modules'],
+    folder: 'www'
+  }, function(err) {
+    if(err) {
+      throw err;
+    }
+    console.log('Published!');
+  });
+
+});
