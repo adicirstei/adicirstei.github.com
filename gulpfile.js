2f44c01462354bedf222330e21e736caf5b31d45
adicirstei <adicirstei@gmail.com>
2014-05-18 14:11:09 +0300

    some jade

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,25 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    jade = require('gulp-jade');
 
 var buildBranch = require('./buildbranch');
 
 
 
-gulp.task('default', function() {
-  // place code for your default task here
-});
+gulp.task('default', ['templates', 'buildbranch']);
 
 
 gulp.task('build', function() {
   // place code for your default task here
+});
+
+
+gulp.task('templates', function() {
+  var YOUR_LOCALS = {};
+  gulp.src('src/**/*.jade')
+  .pipe(jade({
+      locals: YOUR_LOCALS
+  }))
+  .pipe(gulp.dest('./www'));
 });
 
 gulp.task('buildbranch', function() {
@@ -18,7 +27,8 @@ gulp.task('buildbranch', function() {
   buildBranch({
     branch: 'master',
     ignore: ['.git', 'www', 'node_modules'],
-    folder: 'www'
+    folder: 'www',
+    cwd: '.'
   }, function(err) {
     if(err) {
       throw err;
