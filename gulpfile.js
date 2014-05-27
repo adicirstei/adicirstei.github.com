91a4d8b89f3222ff73767c5aeede48c4a2e141cc
Adrian <adicirstei@gmail.com>
2014-05-27 12:40:01 +0300

    data/posts.json

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@
 var gulp = require('gulp'),
   jade = require('gulp-jade'),
   tap = require('gulp-tap'),
-  rename = require('gulp-rename'),
   path = require('path'),
   posts = [],
   marked = require('marked');
